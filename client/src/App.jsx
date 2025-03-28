89f1492225b5dad40bf40388e60db2efddcb4590
Nut.Thakit <natthakitzn@gmail.com>
2025-03-28 14:59:39 +0700

    chore(router): configure BrowserRouter as Router, Route, and Routes in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return <div className="App">{
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </Router>
+  }</div>;
 }
 
 export default App;
